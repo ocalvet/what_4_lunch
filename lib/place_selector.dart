0874e2ac8a9d1a6a3fff749e56eb793853c4dfc6
Ovidio R. Calvet <calveto@gmail.com>
2019-01-14 17:05:55 -0500

    Extract function to navigate to place

--- lib/place_selector.dart
+++ lib/place_selector.dart
@@ -71,40 +71,42 @@ class _PlaceSelectorState extends State<PlaceSelector> {
       content: Text('Yes, we are going to ${generatedPlace.name}'),
       action: SnackBarAction(
         label: 'Map',
-        onPressed: () async {
-          print('One day we will open a map here with directions');
-          String origin =
-              "901 Penninsula corp drive boca raton fl"; // lat,long like 123.34,68.56
-          // String destination="someEndLocationStringAddress or lat,long";
-          String destination = '${generatedPlace.name}';
-          if (LocalPlatform().isAndroid) {
-            final AndroidIntent intent = new AndroidIntent(
-                action: 'action_view',
-                data: Uri.encodeFull(
-                    "https://www.google.com/maps/dir/?api=1&origin=" +
-                        origin +
-                        "&destination=" +
-                        destination +
-                        "&travelmode=driving&dir_action=navigate"),
-                package: 'com.google.android.apps.maps');
-            intent.launch();
-          } else {
-            String url = Uri.encodeFull(
-                "https://www.google.com/maps/dir/?api=1&origin=" +
-                    origin +
-                    "&destination=" +
-                    destination +
-                    "&travelmode=driving&dir_action=navigate");
-            if (await canLaunch(url)) {
-              await launch(url);
-            } else {
-              throw 'Could not launch $url';
-            }
-          }
-        },
+        onPressed: _navigateToPlace,
       ),
     );
     Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  _navigateToPlace() async {
+    print('One day we will open a map here with directions');
+    String origin =
+        "901 Penninsula corp drive boca raton fl"; // lat,long like 123.34,68.56
+    // String destination="someEndLocationStringAddress or lat,long";
+    String destination = '${generatedPlace.name}';
+    if (LocalPlatform().isAndroid) {
+      final AndroidIntent intent = new AndroidIntent(
+          action: 'action_view',
+          data: Uri.encodeFull(
+              "https://www.google.com/maps/dir/?api=1&origin=" +
+                  origin +
+                  "&destination=" +
+                  destination +
+                  "&travelmode=driving&dir_action=navigate"),
+          package: 'com.google.android.apps.maps');
+      intent.launch();
+    } else {
+      String url = Uri.encodeFull(
+          "https://www.google.com/maps/dir/?api=1&origin=" +
+              origin +
+              "&destination=" +
+              destination +
+              "&travelmode=driving&dir_action=navigate");
+      if (await canLaunch(url)) {
+        await launch(url);
+      } else {
+        throw 'Could not launch $url';
+      }
+    }
   }
 
   Widget _bigText(String text, BuildContext context) {
