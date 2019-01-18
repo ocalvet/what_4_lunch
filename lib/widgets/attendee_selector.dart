c9a70cc396c125bdf3cd37608d8bc95e1169e380
Ovidio R. Calvet <calveto@gmail.com>
2019-01-17 23:10:47 -0500

    Add badge to show how many are going

--- lib/widgets/attendee_selector.dart
+++ lib/widgets/attendee_selector.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:badge/badge.dart';
+import 'package:what_4_lunch/blocs/bloc.dart';
+import 'package:what_4_lunch/blocs/bloc_provider.dart';
+import 'package:what_4_lunch/models/attendee.dart';
 
 class AttendeeSelector extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    ApplicationBloc bloc = BlocProvider.of<ApplicationBloc>(context);
     return Container(
-      child: RaisedButton.icon(
-        icon: Icon(Icons.people),
-        onPressed: () {
-          Navigator.pushNamed(context, '/attendees');
-        }, 
-        label: Text('Set Attendees'),
+      child: StreamBuilder<List<Attendee>>(
+        stream: bloc.attendees$,
+        builder: (BuildContext context, AsyncSnapshot<List<Attendee>> snapshot) {
+          return Badge(
+            positionTop: -7,
+            positionRight: -7,
+            color: Colors.green,
+            value: snapshot.data.where((a) => a.attending).length.toString(), // value to show inside the badge
+            child: RaisedButton.icon(
+              icon: Icon(Icons.people),
+              onPressed: () {
+                Navigator.pushNamed(context, '/attendees');
+              },
+              label: Text('Attendees'),
+            ),
+          );
+        },
       ),
     );
   }
