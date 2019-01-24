b48817c977d74b4b0780a7df065135d83e441b68
Ovidio R. Calvet <calveto@gmail.com>
2019-01-24 18:15:08 -0500

    Create decision maker screen

---
+++ lib/screens/make_decision_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MakeDecisionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Decision'),
+      ),
+      body: Container(
+        child: Text('Decision maker'),
+      ),
+    );
+  }
+}
