5cbd3506703b3b1885113c5aed80563c0b9cc42a
Ovidio R. Calvet <calveto@gmail.com>
2019-01-25 14:16:30 -0500

    Copy decision helper screen

--- lib/screens/make_decision_screen.dart
+++ lib/screens/make_decision_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:what_4_lunch/widgets/attendee_selector.dart';
+import 'package:what_4_lunch/widgets/next_meeting_selection.dart';
+import 'package:what_4_lunch/widgets/place_selector.dart';
+import 'package:what_4_lunch/widgets/weather_display.dart';
 
 class MakeDecisionScreen extends StatelessWidget {
   @override
@@ -7,8 +11,41 @@ class MakeDecisionScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Decision'),
       ),
-      body: Container(
-        child: Text('Decision maker'),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned(
+            child: WeatherDisplay(),
+            top: 10,
+            right: 10,
+          ),
+          Positioned(
+            left: 20,
+            top: 25,
+            child: NextMeetingSelection(),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              AttendeeSelector(),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+              ),
+              PlaceSelector(),
+            ],
+          ),
+          Positioned(
+            child: RaisedButton(
+              child: Text('Select'),
+              onPressed: () {
+                // bloc.makeDecision();
+                print('Making decision');
+              },
+            ),
+            left: 20,
+            bottom: 20,
+          )
+        ],
       ),
     );
   }
