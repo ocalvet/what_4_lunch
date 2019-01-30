e4db9b8337ccbd8e3f10cd7bee60e501fb728ea1
Ovidio Calvet <calveto@gmail.com>
2019-01-29 21:44:55 -0500

    Update selecting a place when making a decision

--- lib/screens/make_decision_screen.dart
+++ lib/screens/make_decision_screen.dart
@@ -15,73 +15,88 @@ class MakeDecisionScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Decision'),
       ),
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Positioned(
-            child: WeatherDisplay(),
-            top: 10,
-            right: 10,
-          ),
-          Positioned(
-            left: 20,
-            top: 25,
-            child: NextMeetingSelection(),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              AttendeeSelector(),
-              Padding(
-                padding: EdgeInsets.only(top: 20),
-              ),
-              StreamBuilder<List<Place>>(
-                stream: bloc.places$,
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<Place>> snapshot) {
-                  if (!snapshot.hasData) return CircularProgressIndicator();
-                  return PlaceDropDown(places: snapshot.data);
-                },
-              ),
-            ],
-          ),
-          Positioned(
-            child: RaisedButton(
-              child: Text('Decide'),
-              onPressed: () {
-                bloc.makeDecision(true);
-                Scaffold.of(context).showSnackBar(SnackBar(
-                  content: Text('Decision made'),
-                  backgroundColor: Colors.green,
-                ));
-              },
+      body: DecisionMaker(),
+    );
+  }
+}
+
+class DecisionMaker extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ApplicationBloc bloc = BlocProvider.of<ApplicationBloc>(context);
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Positioned(
+          child: WeatherDisplay(),
+          top: 10,
+          right: 10,
+        ),
+        Positioned(
+          left: 20,
+          top: 25,
+          child: NextMeetingSelection(),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AttendeeSelector(),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
             ),
-            left: 20,
-            bottom: 20,
-          )
-        ],
-      ),
+            PlaceDropDown(),
+          ],
+        ),
+        Positioned(
+          child: RaisedButton(
+            child: Text('Decide'),
+            onPressed: () {
+              bloc.makeDecision(true);
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text('Decision made'),
+                backgroundColor: Colors.green,
+              ));
+            },
+          ),
+          left: 20,
+          bottom: 20,
+        )
+      ],
     );
   }
 }
 
 class PlaceDropDown extends StatelessWidget {
-  final List<Place> places;
-  const PlaceDropDown({Key key, this.places}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     ApplicationBloc bloc = BlocProvider.of<ApplicationBloc>(context);
-    return DropdownButton(
-      items: places.where((p) => p.name != null).map((p) {
-        return DropdownMenuItem(
-          child: Text(p.name),
-          value: p,
+    return StreamBuilder(
+      stream: bloc.places$,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) return CircularProgressIndicator();
+        List<Place> places = snapshot.data;
+        return StreamBuilder(
+          stream: bloc.place$,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) {
+              bloc.selectPlace(places.first);
+              return CircularProgressIndicator();
+            }
+            Place selected = snapshot.data;
+            return DropdownButton<Place>(
+              items: places.where((p) => p.name != null).map((p) {
+                return DropdownMenuItem(
+                  child: Text(p.name),
+                  value: p,
+                );
+              }).toList(),
+              value: selected,
+              onChanged: (Place value) {
+                bloc.selectPlace(value);
+              },
+            );
+          },
         );
-      }).toList(),
-      value: places[0],
-      onChanged: (Place value) {
-        bloc.selectPlace(value);
       },
     );
   }
