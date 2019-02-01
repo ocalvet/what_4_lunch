0195413d9e510db770a7364e1871b86c508e9454
Ovidio R. Calvet <calveto@gmail.com>
2019-02-01 16:35:19 -0500

    Extract UpDown component

--- lib/widgets/place_selector.dart
+++ lib/widgets/place_selector.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:what_4_lunch/blocs/bloc.dart';
 import 'package:what_4_lunch/blocs/bloc_provider.dart';
 import 'package:what_4_lunch/models/place.dart';
-import 'package:android_intent/android_intent.dart';
-import 'package:platform/platform.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:what_4_lunch/widgets/up_down.dart';
 
 class PlaceSelector extends StatelessWidget {
   @override
@@ -25,7 +23,7 @@ class PlaceSelector extends StatelessWidget {
           widgets.add(Padding(
             padding: EdgeInsets.only(top: 10),
           ));
-          widgets.add(_upDown(context, generatedPlace, bloc));
+          widgets.add(UpDown(generatedPlace: generatedPlace,));
         }
         return GestureDetector(
           child: Padding(
@@ -42,72 +40,72 @@ class PlaceSelector extends StatelessWidget {
     );
   }
 
-  Widget _upDown(
-      BuildContext context, Place generatedPlace, ApplicationBloc bloc) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        IconButton(
-          iconSize: 64,
-          color: Colors.green,
-          icon: Icon(Icons.thumb_up),
-          onPressed: () => _selectPlace(generatedPlace, context, bloc),
-        ),
-        IconButton(
-          iconSize: 64,
-          color: Colors.red,
-          icon: Icon(Icons.thumb_down),
-          onPressed: () => _rejectPlace(bloc),
-        )
-      ],
-    );
-  }
+  // Widget _upDown(
+  //     BuildContext context, Place generatedPlace, ApplicationBloc bloc) {
+  //   return Row(
+  //     mainAxisAlignment: MainAxisAlignment.center,
+  //     children: <Widget>[
+  //       IconButton(
+  //         iconSize: 64,
+  //         color: Colors.green,
+  //         icon: Icon(Icons.thumb_up),
+  //         onPressed: () => _selectPlace(generatedPlace, context, bloc),
+  //       ),
+  //       IconButton(
+  //         iconSize: 64,
+  //         color: Colors.red,
+  //         icon: Icon(Icons.thumb_down),
+  //         onPressed: () => _rejectPlace(bloc),
+  //       )
+  //     ],
+  //   );
+  // }
 
-  _rejectPlace(ApplicationBloc bloc) async {
-    bloc.makeDecision(false);
-    bloc.getRandomPlace();
-  }
+  // _rejectPlace(ApplicationBloc bloc) async {
+  //   bloc.makeDecision(false);
+  //   bloc.getRandomPlace();
+  // }
 
-  _selectPlace(Place generatedPlace, BuildContext context, ApplicationBloc bloc) async {
-    bloc.makeDecision(true);
-    final snackBar = SnackBar(
-      content: Text('Yes, we are going to ${generatedPlace.name}'),
-      action: SnackBarAction(
-        label: 'Map',
-        onPressed: () => _navigateToPlace(generatedPlace),
-      ),
-    );
-    Scaffold.of(context).showSnackBar(snackBar);
-  }
+  // _selectPlace(Place generatedPlace, BuildContext context, ApplicationBloc bloc) async {
+  //   bloc.makeDecision(true);
+  //   final snackBar = SnackBar(
+  //     content: Text('Yes, we are going to ${generatedPlace.name}'),
+  //     action: SnackBarAction(
+  //       label: 'Map',
+  //       onPressed: () => _navigateToPlace(generatedPlace),
+  //     ),
+  //   );
+  //   Scaffold.of(context).showSnackBar(snackBar);
+  // }
 
-  _navigateToPlace(Place generatedPlace) async {
-    String origin = "901 Penninsula corp drive boca raton fl";
-    String destination = '${generatedPlace.name}';
-    if (LocalPlatform().isAndroid) {
-      final AndroidIntent intent = new AndroidIntent(
-          action: 'action_view',
-          data: Uri.encodeFull(
-              "https://www.google.com/maps/dir/?api=1&origin=" +
-                  origin +
-                  "&destination=" +
-                  destination +
-                  "&travelmode=driving&dir_action=navigate"),
-          package: 'com.google.android.apps.maps');
-      intent.launch();
-    } else {
-      String url = Uri.encodeFull(
-          "https://www.google.com/maps/dir/?api=1&origin=" +
-              origin +
-              "&destination=" +
-              destination +
-              "&travelmode=driving&dir_action=navigate");
-      if (await canLaunch(url)) {
-        await launch(url);
-      } else {
-        throw 'Could not launch $url';
-      }
-    }
-  }
+  // _navigateToPlace(Place generatedPlace) async {
+  //   String origin = "901 Penninsula corp drive boca raton fl";
+  //   String destination = '${generatedPlace.name}';
+  //   if (LocalPlatform().isAndroid) {
+  //     final AndroidIntent intent = new AndroidIntent(
+  //         action: 'action_view',
+  //         data: Uri.encodeFull(
+  //             "https://www.google.com/maps/dir/?api=1&origin=" +
+  //                 origin +
+  //                 "&destination=" +
+  //                 destination +
+  //                 "&travelmode=driving&dir_action=navigate"),
+  //         package: 'com.google.android.apps.maps');
+  //     intent.launch();
+  //   } else {
+  //     String url = Uri.encodeFull(
+  //         "https://www.google.com/maps/dir/?api=1&origin=" +
+  //             origin +
+  //             "&destination=" +
+  //             destination +
+  //             "&travelmode=driving&dir_action=navigate");
+  //     if (await canLaunch(url)) {
+  //       await launch(url);
+  //     } else {
+  //       throw 'Could not launch $url';
+  //     }
+  //   }
+  // }
 
   Widget _bigText(String text, BuildContext context) {
     TextStyle textStyle = Theme.of(context).textTheme.display1;
---
+++ lib/widgets/up_down.dart
@@ -0,0 +1,81 @@
+import 'package:android_intent/android_intent.dart';
+import 'package:flutter/material.dart';
+import 'package:platform/platform.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:what_4_lunch/blocs/bloc.dart';
+import 'package:what_4_lunch/blocs/bloc_provider.dart';
+import 'package:what_4_lunch/models/place.dart';
+
+class UpDown extends StatelessWidget {
+  final Place generatedPlace;
+
+  const UpDown({Key key, this.generatedPlace}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    ApplicationBloc bloc = BlocProvider.of<ApplicationBloc>(context);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        IconButton(
+          iconSize: 64,
+          color: Colors.green,
+          icon: Icon(Icons.thumb_up),
+          onPressed: () => _selectPlace(generatedPlace, context, bloc),
+        ),
+        IconButton(
+          iconSize: 64,
+          color: Colors.red,
+          icon: Icon(Icons.thumb_down),
+          onPressed: () => _rejectPlace(bloc),
+        )
+      ],
+    );
+  }
+
+  _rejectPlace(ApplicationBloc bloc) async {
+    bloc.makeDecision(false);
+    bloc.getRandomPlace();
+  }
+
+  _selectPlace(Place generatedPlace, BuildContext context, ApplicationBloc bloc) async {
+    bloc.makeDecision(true);
+    final snackBar = SnackBar(
+      content: Text('Yes, we are going to ${generatedPlace.name}'),
+      action: SnackBarAction(
+        label: 'Map',
+        onPressed: () => _navigateToPlace(generatedPlace),
+      ),
+    );
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  _navigateToPlace(Place generatedPlace) async {
+    String origin = "901 Penninsula corp drive boca raton fl";
+    String destination = '${generatedPlace.name}';
+    if (LocalPlatform().isAndroid) {
+      final AndroidIntent intent = new AndroidIntent(
+          action: 'action_view',
+          data: Uri.encodeFull(
+              "https://www.google.com/maps/dir/?api=1&origin=" +
+                  origin +
+                  "&destination=" +
+                  destination +
+                  "&travelmode=driving&dir_action=navigate"),
+          package: 'com.google.android.apps.maps');
+      intent.launch();
+    } else {
+      String url = Uri.encodeFull(
+          "https://www.google.com/maps/dir/?api=1&origin=" +
+              origin +
+              "&destination=" +
+              destination +
+              "&travelmode=driving&dir_action=navigate");
+      if (await canLaunch(url)) {
+        await launch(url);
+      } else {
+        throw 'Could not launch $url';
+      }
+    }
+  }
+}
